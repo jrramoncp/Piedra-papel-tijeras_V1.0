ea8bfe126602182999d84392ce43b6675f80367e
AchingRCP <joseramoncaravaca@gmail.com>
2024-05-14 10:11:47 +0200

    Añado condicional  de aleatoriedad, y la divido en tres rangos para que la consola devuelva una string determinada

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+//La consola tiene que dar tres valores aleatorios
+//Para cada valor hay que asignar un texto piedra papel o tijeras
+//
+
+let x = Math.floor(Math.random()*10)
+
+if (x >= 0 && x <= 2){
+    console.log("rock")
+    }
+else if(x >= 3 && x <= 5){
+    console.log("papper")
+}
+else if (x >= 6 && x <= 9){
+    console.log("scissors")
+}
+        
